132c7e45b6808b4c783e90a3d34c2fe2078138f3
Yll Gajtani <123652630+Yllgajtani@users.noreply.github.com>
2025-10-02 18:48:49 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Qytetet from './Qytetet'
+
+function App() {
+
+
+  return (
+    <>
+     <Qytetet/>
+    </>
+  )
+}
+
+export default App
